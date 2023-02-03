3de8a906ad043729b283dcb65a246f3e568e9e90
Noah Cowan <nsncowan@gmail.com>
2023-02-03 15:00:09 -0800

    boilerplate files for C# MSTest projects

---
+++ ProjectName.Tests/ModelTests/ClassNameTests.cs
@@ -0,0 +1,25 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProjectName;
+
+namespace ProjectName.Tests
+{
+  [TestClass]
+  public class ClassNameTests
+  {
+    
+  }
+}
+
+
+// Example test format:
+
+/*
+
+[TestMethod]
+public void NameOfMethodWeAreTesting_DescriptionOfBehavior_ExpectedReturnValue()
+{
+  // any necessary logic to prep for test; instantiating new classes, etc.
+  Assert.AreEqual(EXPECTED RESULT, CODE TO TEST);
+}
+
+ */
